ef21a1484bb978d6964d9779e6b11e2023364c28
lenovo <18235815434@163.com>
2018-07-20 15:31:05 +0800

    tijiao

--- js/index.js
+++ js/index.js
@@ -26,12 +26,9 @@
                 "            <li><a href='#'>免费服务</a></li>\n" +
                 "            <li><a href='#'>收费服务</a></li>\n" +
                 "        </ul>");
-            $("a").css({
-                color: "white"
-            });
         }
         if (this.className=="campus_join"){
-            $(".campus_join img").attr('src','image/安全招聘2.png');
+            $(".campus_join img").attr('src','image/校园合作2.png');
             $(".choose-menu").append("<ul>\n" +
                 "            <li>选项</li>\n" +
                 "            <li>选项</li>\n" +
@@ -40,7 +37,7 @@
                 "        </ul>");
         }
         if (this.className=="safe_job"){
-            $(".safe_job img").attr('src','image/校园合作2.png');
+            $(".safe_job img").attr('src','image/安全招聘2.png');
             $(".choose-menu").append("<ul>\n" +
                 "            <li>选项</li>\n" +
                 "            <li>选项</li>\n" +
@@ -97,17 +94,45 @@
             $(".campus_join img").attr('src','image/安全招聘.png');
             $(".choose-menu").empty();
         }
-        if (this.className=="safe_job"){
-            $(".safe_job img").attr('src','image/校园合作.png');
-            $(".choose-menu").empty();
-        }
-        if (this.className=="safe_train"){
-            $(".safe_train img").attr('src','image/安全培训.png');
-            $(".choose-menu").empty();
-        }
-        if (this.className=="safe_forum"){
-            $(".safe_forum img").attr('src','image/安全论坛.png');
-            $(".choose-menu").empty();
-        }
+        $(".first_page, .free_server, .value_add_server, .campus_join, .safe_job, .safe_train, .safe_forum").mouseout(function () {
+            $(this).css({
+                color: "white",
+                background: "#17181f"
+            });
+            $(this).find("img").css({
+                background: ""
+            });
+            $(".choose-menu").css({
+                display: "none"
+            });
+            if (this.className=="first_page"){
+                $(".first_page img").attr('src','image/首页.png');
+                // $(".choose-menu").empty();
+            }
+            if (this.className=="free_server"){
+                $(".free_server img").attr('src','image/免费服务.png');
+                $(".choose-menu").empty();
+            }
+            if (this.className=="value_add_server"){
+                $(".value_add_server img").attr('src','image/增值服务.png');
+                $(".choose-menu").empty();
+            }
+            if (this.className=="campus_join"){
+                $(".campus_join img").attr('src','image/安全招聘.png');
+                $(".choose-menu").empty();
+            }
+            if (this.className=="safe_job"){
+                $(".safe_job img").attr('src','image/校园合作.png');
+                $(".choose-menu").empty();
+            }
+            if (this.className=="safe_train"){
+                $(".safe_train img").attr('src','image/安全培训.png');
+                $(".choose-menu").empty();
+            }
+            if (this.className=="safe_forum"){
+                $(".safe_forum img").attr('src','image/安全论坛.png');
+                $(".choose-menu").empty();
+            }
+        });
     });
 }
